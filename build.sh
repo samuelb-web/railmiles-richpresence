7c9915e79adfb84b5d210f379c49a2fc6e35db4b
Samuel B <sam@thelongwayround.co.uk>
2024-02-14 15:52:58 +0000

    comment out the mac ones as they're not supported + add noconsole

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir -p out
 
-GOOS=linux GOARCH=amd64  go build -o ./out/railmiles-richpresence.out
-GOOS=darwin GOARCH=amd64 go build -o ./out/railmiles-richpresence.mac_intel.out
-GOOS=darwin GOARCH=arm64 go build -o ./out/railmiles-richpresence.mac_m1.out
-GOOS=windows GOARCH=amd64 go build -o ./out/railmiles-richpresence.exe
+GOOS=linux GOARCH=amd64  go build -o -ldflags -H=windowsgui ./out/railmiles-richpresence.out
+#GOOS=darwin GOARCH=amd64 go build -o ./out/railmiles-richpresence.mac_intel.out
+#GOOS=darwin GOARCH=arm64 go build -o ./out/railmiles-richpresence.mac_m1.out
+GOOS=windows GOARCH=amd64 go build -o -ldflags -H=windowsgui ./out/railmiles-richpresence.exe
