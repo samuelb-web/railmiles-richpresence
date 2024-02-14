be9e098d1f4ca5743b504a437c4db9e5de32add7
Samuel B <sam@thelongwayround.co.uk>
2024-02-14 16:01:25 +0000

    nope that was wrong i fixed it now

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-mkdir -p out
+﻿mkdir -p out
 
-GOOS=linux GOARCH=amd64  go build -o -ldflags -H=windowsgui ./out/railmiles-richpresence.out
+GOOS=linux GOARCH=amd64  go build -o ./out/railmiles-richpresence.out
 #GOOS=darwin GOARCH=amd64 go build -o ./out/railmiles-richpresence.mac_intel.out
 #GOOS=darwin GOARCH=arm64 go build -o ./out/railmiles-richpresence.mac_m1.out
-GOOS=windows GOARCH=amd64 go build -o -ldflags -H=windowsgui ./out/railmiles-richpresence.exe
+GOOS=windows GOARCH=amd64 go build -o ./out/railmiles-richpresence.exe -ldflags="-H windowsgui"
+
+
